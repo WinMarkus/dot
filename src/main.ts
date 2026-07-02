f9cb106150c9e3424dd1fd609d13b79a0aab7a8f
Markus Wagner <k12243132@students.jku.at>
2026-07-02 07:08:22 +0200

    Import mobile command bar fixes

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './styles.css';
 import './delete-markers.css';
+import './mobile-command-bar.css';
 
 createApp(App).mount('#app');

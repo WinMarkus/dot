34df00c79038d57ce0e5621c7c19b6a62be380eb
Markus Wagner <k12243132@students.jku.at>
2026-07-03 07:48:18 +0200

    Extract app constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const ARTIFACT_WIDTH = 320;
+export const ARTIFACT_HEIGHT = 230;
+export const MIN_ZOOM = 0.35;
+export const MAX_ZOOM = 2.4;
+export const DELETE_TRANSITION_MS = 560;
+export const NEST_EXTRA_HEIGHT = 82;
